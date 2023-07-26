ce5c14b5657164c4032f588c69f1212220fbcc5d
iOS-Yetti <yechan123456@gmail.net>
2023-07-26 13:49:33 +0900

    feat: MacBook 구조체 생성

---
+++ POPPractice/MacBook.swift
@@ -0,0 +1,18 @@
+//
+//  MacBook.swift
+//  POPPractice
+//
+//  Created by 예찬 on 2023/07/26.
+//
+
+struct MacBook {
+    let allowedChargeWattHour: Watt
+    let currentBatteryCapacity: Watt
+    let MaximumBatteryCapacity: Watt
+    
+    func chargeBattery(charger: Chargeable) {
+        print("충전까지 걸린 시간: ")
+        
+    }
+    
+}
